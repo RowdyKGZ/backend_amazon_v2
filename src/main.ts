33d6dd4f297b393d00e2a365d2fa481f9b0cf53a
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-10-11 13:53:17 +0600

    end login auth

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    {
+      cors: true,
+      logger: ['error', 'warn', 'log'], // <--- Add this line in options object
+    },
+  );
 
   app.setGlobalPrefix('api');
   app.enableCors();
